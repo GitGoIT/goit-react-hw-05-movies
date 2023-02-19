7e19fd0d600822918a17e98afef69f3e8a7e9fb0
Ivan Alimenko <ivan.alimenko@ukr.net>
2023-02-20 00:41:29 +0200

    commit_App-upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,6 @@ import { UserRoutes } from './UserRoutes';
 export const App = () => {
   return (
     <BrowserRouter
-      basename="/goit-react-hw-05-movies"
       style={{
         height: '100vh',
         display: 'flex',
